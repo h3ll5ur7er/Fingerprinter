00fb7ef0c64334b51722a847b840b6815b6ab9e9
Emanuel Knecht <emanuel.knecht@scs.ch>
2023-05-16 08:01:02 +0200

    Implement serial port selection + improve logging

--- Program.cs
+++ Program.cs
@@ -5,13 +5,38 @@ using System.Threading.Tasks;
 namespace Fingerprinter;
 
 public class Program {
-    public static string Port = SafeSerialPort.GetPortNames().LastOrDefault() ?? throw new Exception("No serial ports found.");
+    public static string[] Ports = SafeSerialPort.GetPortNames();
+    public static string? Port;
     public static int BaudRate = 57600;
 
     public static void Main(string  [] args) {
         Run().Wait();
     }
     public static async Task Run() {
+        if (Ports.Length == 0) {
+            Logger.Write("No Serial ports found!", level: LogLevel.None);
+            return;
+        } else if (Ports.Length == 1) {
+            Port = Ports[0];
+        } else {
+            Logger.Write($"Please select the correct Serial port:", level: LogLevel.None);
+            for (int i = 0; i < Ports.Length; i++)
+            {
+                Logger.Write($"[{i+1}]  {Ports[i]}", level: LogLevel.None);
+            }
+            while(Port == null){
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out var portIndex) && portIndex > 0 && portIndex <= Ports.Length) {
+                    Port = Ports[portIndex-1];
+                } else {
+                    Logger.Write("Invalid input! Enter the number of the port you want to use.", level: LogLevel.None);
+                }
+            }
+        }
+        Logger.Write("Using port: " + Port, level: LogLevel.None);
+
+
+
         try {
             await FingerprintService.Instance.Connect();
             var counter = 0;
@@ -28,9 +53,9 @@ public class Program {
                 }
                 await Task.Delay(100);
             }
-            Logger.Write("Bye!");
+            Logger.Write("Bye!", level: LogLevel.None);
         } catch (Exception e) {
-            Logger.Write("Unexpected error occured: " + e.ToString());
+            Logger.Write("Unexpected error occured: " + e.ToString(), level: LogLevel.None);
         }
     }
 }
